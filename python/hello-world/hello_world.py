ca22668f482ecb7be04db5bf450debce54dae49b
None <msayed@localhost.localdomain>
2015-05-17 07:04:24 +0200

    solved hello world and passed all tests

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -2,5 +2,10 @@
 # Skeleton file for the Python "Hello World" exercise.
 #
 def hello(name = ''):
+    if name == "":
+        return "Hello, world!"
+    else:
+        return "Hello, " + name +"!"
 
-    return
+
+
