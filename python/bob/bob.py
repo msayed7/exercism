174a76a588adf37867733b85e12566265141f08c
None <msayed@localhost.localdomain>
2015-05-18 13:44:33 +0200

    finished bob problem

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -3,4 +3,14 @@
 #
 def hey(what):
 
-    return
+
+    if what.strip() == "":
+        return 'Fine. Be that way!'
+    elif what.isupper() == True:
+        return 'Whoa, chill out!'
+    #split sentence into words and get last char of last word 
+    elif what.split()[-1][-1] == "?":
+        return 'Sure.'
+
+    return 'Whatever.'
+
